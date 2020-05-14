e990c0df3440052cf7f8dd8debad8d8027eb3887
huiming chen <v-huchen@expedia.com>
2020-05-14 18:30:38 +0800

    create quiz

--- src/main/java/com/heiio/book/quiz/thread/ThreadLocalDateUtilTwo.java
+++ src/main/java/com/heiio/book/quiz/thread/ThreadLocalDateUtilTwo.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ThreadLocalDateUtilTwo {
-    private static final String date_format = "yyyy-MM-dd HH:mm:ss";
+    public static final String date_format = "yyyy-MM-dd HH:mm:ss";
     private static ThreadLocal<DateFormat> threadLocal = new ThreadLocal<DateFormat>() {
         @Override
         protected DateFormat initialValue() {
@@ -16,7 +16,7 @@ public class ThreadLocalDateUtilTwo {
         }
     };
 
-    private static ThreadLocal<DateFormat> threadLocal_temp = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    public static ThreadLocal<DateFormat> threadLocal_temp = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
 
     public static Date parse(String dateStr) throws ParseException {
         return threadLocal_temp.get().parse(dateStr);
