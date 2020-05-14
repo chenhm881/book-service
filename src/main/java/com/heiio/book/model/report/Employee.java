85dac34b4f5ecbe9837635c758af8e203af76aa3
huiming chen <v-huchen@expedia.com>
2020-05-14 10:00:41 +0800

    Rmove @Getter @Setter once @Data exists

--- src/main/java/com/heiio/book/model/report/Employee.java
+++ src/main/java/com/heiio/book/model/report/Employee.java
@@ -6,15 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
 @AllArgsConstructor
 @EqualsAndHashCode
 @Builder
-@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor(force = true)
 @Data
